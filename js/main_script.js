a4d21000c3b3b35c2c927d268fb64a65959df61a
runninbi <wisemushrooman@gmail.com>
2023-06-20 11:57:57 +0900

    feat: 스크롤 이벤트 추가 (230619)

--- js/main_script.js
+++ js/main_script.js
@@ -1,6 +1,62 @@
 $(document).ready(function(){
 	/* index.html */
 
+	//scroll action
+	$(function(){
+		var scrollTop = $(window).scrollTop();
+		   $('.searchicon').click(function(){
+			   var captionTop = $('.where_to_go').offset().top;
+			   $('html, body').animate({scrollTop : captionTop}, 500);
+			   $(".keyboard_area").css('display','none');
+			   $(".take_contents").css('display','block');
+			   $(".where_to_go").addClass('addshadow');
+			   $(".where_to_go_title_box h2").css('display','none');
+			   $(".top_button").css('display','block');
+			   return false;
+			});
+	   });
+
+	   $(".form_w100").click(function(){
+		window.scrollTo({top : 0, behavior: 'smooth'}); 
+		$(".where_to_go_title_box h2").css('display','block');
+		$(".top_button").css('display','none');
+	  });
+
+	   //스크롤 시 fixed
+	   $(function(){
+			scrollAct();
+		})
+	
+		function scrollAct(){
+			var position = $(".where_to_go").offset().top; //fixed 메뉴 위치
+	
+			$(window).on('scroll', function() {
+				var scrollTop = $(window).scrollTop(); //현재 스크롤 위치
+				var topsectionH = $('.footer').height(); //footer 높이
+				var wrapH = $('#wrap').height(); //모든 컨텐츠 높이
+				var conH = wrapH - topsectionH - $('.where_to_go').height() - position; //footer 까지 스크롤 도달 위치 계산
+		
+				if (scrollTop > position){ 
+					if (conH + position - 345 > scrollTop){
+						$('.where_to_go').css({'position':'fixed', 'top':'0px'})
+						$(".where_to_go_title_box h2").css('display','none');
+			   			$(".top_button").css('display','block');
+					}else {
+						$('.where_to_go').css({'position':'absolute','top':conH})
+					}   
+				}else{
+					$('.where_to_go').css({'position':'absolute','top':'345px'})
+				}
+			})
+	}   
+
+	//top_button
+	$(".top_button").click(function(){
+		window.scrollTo({top : 0, behavior: 'smooth'}); 
+		$(".where_to_go_title_box h2").css('display','block');
+		$(".top_button").css('display','none');
+	  });
+
 	//stop_info_slider
 	$(".stop_info_slider").slick({
 		autoplay:false,
